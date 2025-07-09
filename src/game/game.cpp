28730e35cc800e9d945f6061399847e69e4404d2
Joar Axås <jgoeransson@ethz.ch>
2025-07-09 06:52:21 +0200

    rearrange initialization of systems to make input last

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -15,13 +15,13 @@ Game::Game()
 {
 	gamename = "Name of the game";
 	timer = std::make_unique<TimeManager>();
-	input = std::make_unique<InputManager>(this);
 	cam = std::make_unique<Camera>(this);
 	rendering = std::make_unique<Rendering>(this, cam.get());
 	ui = std::make_unique<InterfaceManager>(this);
 	allsprites = std::make_unique<SpriteManager>(this);
 	resources = std::make_unique<ResourceManager>(this);
 	gamestate = std::make_unique<Gamestate>(this);
+	input = std::make_unique<InputManager>(this);
 	quit = false;
 }
 
