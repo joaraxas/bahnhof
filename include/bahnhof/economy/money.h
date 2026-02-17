e966cc34d3632c86ee192b044b05b18a07d5de2b
Joar Axås <joar.axas@gmail.com>
2026-02-17 19:10:22 +0100

    change money to double

--- include/bahnhof/economy/money.h
+++ include/bahnhof/economy/money.h
@@ -4,12 +4,11 @@
 class Money
 {
 public:
-    constexpr Money() {Money{0};}
-    constexpr Money(int v) : amount{std::max(0,v)} {}
-    constexpr Money(double v) : amount{std::lround(v)} {}
+    constexpr Money() {Money{0.0};}
+    constexpr Money(double v) : amount{v} {}
 
     Money operator+=(const Money other) {amount+=other.amount; return *this;}
-    Money operator-=(const Money other) {amount-=other.amount; if(amount<0) amount=0; return *this;}
+    Money operator-=(const Money other) {amount-=other.amount; return *this;}
     
     bool operator<(const Money other) const {return amount<other.amount;};
     bool operator>(const Money other) const {return amount>other.amount;};
@@ -18,12 +17,12 @@ public:
     bool operator==(const Money other) const {return amount==other.amount;};
     bool operator!=(const Money other) const {return amount!=other.amount;};
 
-    operator std::string() const {return std::to_string(amount)+" Fr";};
+    operator std::string() const {return std::format("{0:.2f} Fr", amount);};
     friend std::ostream& operator<<(std::ostream& os, const Money m) {return os << std::string(m);};
 
     friend float operator/(Money lhs, const float rhs) {return lhs.amount/rhs;}
 private:
-    long amount;
+    double amount;
 };
 
 inline Money operator+(Money lhs, const Money rhs) {return lhs+=rhs;}
