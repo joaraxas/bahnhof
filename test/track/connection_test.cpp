e3f8ff544802383bb21fd0af725e50b504143659
Joar Axås <joar.axas@gmail.com>
2026-06-28 09:48:29 +0200

    add test for horizontal track connection

--- test/track/connection_test.cpp
+++ test/track/connection_test.cpp
@@ -1,6 +1,5 @@
 #include "bahnhof/common/forwardincludes.h"
 #include "bahnhof/track/track.h"
-#include "bahnhof/track/trackinternal.h"
 #include "bahnhof/graphics/graphics.h"
 #include "bahnhof/common/gamestate.h"
 
@@ -12,21 +11,17 @@ int main(int argv, char** argc) {
     Game game{};
 
     Tracksystem& tracks = game.getgamestate().gettracksystems();
-    // tracks.buildfromcoords({{500,400}, 
-    //                         {400,400}});
     Angle angle;
     Tracksection s = Input::planconstructionto(tracks, Vec{500,400}, Vec{900,400});
-    std::cout<<s.nodes.front()->getpos()<<std::endl;
-    std::cout<<s.nodes.back()->getpos()<<std::endl;
     Input::buildsection(tracks, s);
     s = Input::planconstructionto(tracks, Vec{500,400}, 400, angle);
-    std::cout<<angle<<std::endl;
-    std::cout<<s.nodes.back()->getpos()<<std::endl;
     Input::buildsection(tracks, s);
-    // auto s = Input::planconstructionto(tracks, Vec{400,400}, Vec{400,500});
-    // Input::buildsection(tracks, s);
-    // s = Input::planconstructionto(tracks, Input::getendpointat(tracks, Vec{400,400}), Vec{400,300});
-    // Input::buildsection(tracks, s);
+    
+    Vec a{700,500};
+    s = Input::planconstructionto(tracks, a, Vec{900,500});
+    Input::buildsection(tracks, s);
+    s = Input::planconstructionto(tracks, tracks.getnode(Input::selectnodeat(tracks, a)), Vec{100,499.9});
+    Input::buildsection(tracks, s);
 
 	game.play();
 	close();
