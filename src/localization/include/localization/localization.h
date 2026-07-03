75095e37e56bb32cfb11c00ec3bbc4d1e9ae89e3
Joar Axås <joar.axas@gmail.com>
2026-07-03 17:56:20 +0200

    add support for string formatting in localization

--- src/localization/include/localization/localization.h
+++ src/localization/include/localization/localization.h
@@ -1,4 +1,11 @@
 #include <string>
+#include <format>
 
 bool loadlanguage(std::string path);
 const std::string& tr(std::string id);
+template<typename... Args>
+std::string tr(std::string id, Args&&... args){
+    return std::vformat(tr(id), 
+        std::make_format_args(args...)
+    );
+}
